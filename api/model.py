4675544c86cdc0a206cfdffac614a1e83b32310c
Ridhima <54638081+RidhimaKohli@users.noreply.github.com>
2022-04-28 02:28:23 +0530

    pickle thing tried

--- api/model.py
+++ api/model.py
@@ -1,17 +1,24 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import pandas as pd
 
-import torch
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix,classification_report,accuracy_score
-from sklearn.preprocessing import LabelEncoder
+# import torch
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import confusion_matrix,classification_report,accuracy_score
+# from sklearn.preprocessing import LabelEncoder
 
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+# from tensorflow.keras.utils import to_categorical
+# from tensorflow.keras.preprocessing.text import Tokenizer
+# from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.layers import Dense, LSTM, Embedding, Bidirectional,Dropout
+# from tensorflow.keras.optimizers import Adam
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.callbacks import EarlyStopping
+# from tensorflow.keras.layers import Dense, LSTM, Embedding, Bidirectional,Dropout
+
+import pickle
+pipelinemodel = pickle.load(open('api//model_pkl.pkl', 'rb'))
+
+x_test = "I feel happy"
+
+print(pipelinemodel.predict(x_test))
