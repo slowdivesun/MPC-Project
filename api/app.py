d76a4e4ae895ffcd7f396e3303ec4dba776c1581
Shivam <shivam.gup82@gmail.com>
2022-04-27 18:41:27 +0530

    Querying using pymongo

--- api/app.py
+++ api/app.py
@@ -1,8 +1,26 @@
 from flask import Flask, jsonify, request
 
+from dotenv import dotenv_values
+from flask_pymongo import pymongo
+import certifi
+
+config = dotenv_values(".env")
+
+ca = certifi.where()
+
+CONNECTION_STRING = config["MONGO_URI"]
+client = pymongo.MongoClient(CONNECTION_STRING, tlsCAFile=ca)
+
+db = client.get_database('Diary')
+
+
 app = Flask(__name__)
 
 @app.route("/diary", methods=["GET"])
-def diary_entry():
-    return "<p>Here is the diary entry</p>"
+def hello():
+    return "Hello"
+    pass
+
+@app.route("/emotion", methods=["POST"])
+def addEmotion(entryId):
     pass
--- api/db.py
+++ api/db.py
@@ -1,14 +1,18 @@
-from flask import Flask
 from dotenv import dotenv_values
 from flask_pymongo import pymongo
 import certifi
+import pprint
 
 config = dotenv_values(".env")
 
 ca = certifi.where()
 
-
 CONNECTION_STRING = config["MONGO_URI"]
 client = pymongo.MongoClient(CONNECTION_STRING, tlsCAFile=ca)
 
-db = client.get_database('Diary')
+db = client.get_database('myFirstDatabase')
+# print(db.list_collection_names())
+
+users = db.get_collection("users")
+for user in users.find():
+    print(user)
