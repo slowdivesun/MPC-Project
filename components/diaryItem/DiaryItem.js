0f515cdf0e555a8e33dd18b0b761c3c1048b66db
Shivam <shivam.gup82@gmail.com>
2022-05-04 22:55:17 +0530

    Modified width/height and added background image

--- components/diaryItem/DiaryItem.js
+++ components/diaryItem/DiaryItem.js
@@ -6,9 +6,15 @@ export default function DiaryItem({ item }) {
   const { text, date, emotion } = item;
   return (
     <TouchableOpacity
-      style={{ flexDirection: "row", justifyContent: "space-between" }}
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: 300,
+        backgroundColor: "white",
+        opacity: 0.7,
+      }}
     >
-      <Text>Entry on {moment(date).format("MMMM Do YYYY")}</Text>
+      <Text>{moment(date).format("MMMM Do YYYY")}</Text>
       <Text>{emotion === 0 ? "Negative" : "Positive"}</Text>
     </TouchableOpacity>
   );
